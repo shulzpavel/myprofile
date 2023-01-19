3b4d1966b92dcb2a97e1b546403373ed63238260
Paul Shults <shulzpavel@gmail.com>
2023-01-19 18:12:29 +0300

    fix: страница профиля

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,16 +6,23 @@ import Profile from '../Profile/Profile'
 import Tech from '../Tech/Tech'
 
 function Main() {
-  return (
-    <div className='main'>
-      <Switch>
-        <Route exact path='/' component={Profile} />
-        <Route exact path='/projects' component={Projects} />
-        <Route exact path='/profile' component={Profile} />
-        <Route exact path='/tech' component={Tech} />
-      </Switch>
-      </div>
-  );
+    return ( <
+        div className = 'main' >
+        <
+        Switch >
+        <
+        Route exact path = '/'
+        component = { Profile }
+        /> <
+        Route exact path = '/projects'
+        component = { Projects }
+        /> <
+        Route exact path = '/profile'
+        component = { Profile }
+        /> { /* <Route exact path='/tech' component={Tech} /> */ } <
+        /Switch> <
+        /div>
+    );
 }
 
 export default Main;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,29 +1,51 @@
 // import me from '../../images/portfolio.jpg';
 // import me from '../../images/photo-me.jpg';
-import React from 'react';
-import me from '../../images/paul.jpg';
+import React from "react";
+import me from "../../images/paul.jpg";
 
 function Profile() {
-    return (
-        <div className='profile' id='about-me'>
-            <a className='profiel__link'>
-                <img className='profile__image' src={me} alt='me' />
-            </a>
-            <p className='profile__title'>Немного информации обо мне</p>
-            <h1 className='profile__subtitle-1'>
-                Мне 28 лет. Я живу в городе Москва!
-                Junior frontend-developer с опытом разработки pet-проектов.
-            </h1>
-            <h1 className='profile__subtitle-2'>
-Нравится создавать вещи, которые живут в Интернете. Интерес к веб-разработке начался еще в 2020 году, когда я решил полностью сменить профессию.
-            </h1>
-            <h1 className='profile__subtitle-3'>
-Основное внимание уделяю созданию доступных продуктов и цифровых услуг для широкого круга клиентов.
-Нравится решать проблемы дизайна, создавать умный пользовательский интерфейс при разработке больших веб-приложений.
-            </h1>
-        </div>
-
-    )
+  return (
+    <div className="profile" id="about-me">
+      <div className="profile__container">
+        <a className="profiel__link">
+          <img className="profile__image" src={me} alt="me" />
+        </a>
+        <p className="profile__title"> Шульц Павел </p>
+      </div>
+      <p className="profile__subtitle"> Juniour Frontend Developer </p>
+      <h1 className="profile__item">
+        Привет! Меня зовут Павел, и мне нравится создавать вещи, которые живут в
+        Интернете. Мой интерес к веб - разработке начался еще в 2020 году, когда
+        я решил полностью сменить профессию.
+      </h1>
+      <h1 className="profile__item">
+        В настоящее время мое основное внимание уделяется созданию доступных
+        продуктов и цифровых услуг для широкого круга клиентов.Мне нравится
+        решать проблемы дизайна, создавать умный пользовательский интерфейс при
+        разработке больших веб - приложений.
+      </h1>
+      <h1 className="profile__item">
+        Когда я не работаю или не вожусь с кодом, я люблю изучать новые
+        технологии в мире IT или ходить на прогулку со своим псом Шелби.
+      </h1>
+      <div className="profile__container">
+        <p className="profile__title"> Skills </p>
+      </div>
+      <div className="profile__stack">
+        <h1 className="profile__item-green">JavaScript</h1>
+        <h1 className="profile__item-green">HTML</h1>
+        <h1 className="profile__item-green">CSS</h1>
+        <h1 className="profile__item-green">React</h1>
+        <h1 className="profile__item-green">Node.js</h1>
+        <h1 className="profile__item-green">Git</h1>
+        <h1 className="profile__item-green">FIgma</h1>
+        <h1 className="profile__item-green">dev tools</h1>
+      </div>
+      {/* <div className="profile__skills">
+        <p className="profile__title"> Софт скилы </p>
+      </div> */}
+    </div>
+  );
 }
 
 export default Profile;
