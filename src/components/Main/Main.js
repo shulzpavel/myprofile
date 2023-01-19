611cd065449502785bf0c4b7ec29d2c90215f1f8
Paul Shults <shulzpavel@gmail.com>
2023-01-19 23:59:15 +0300

    fix: main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,16 +3,16 @@ import { Switch, Route } from "react-router-dom";
 import "./Main.css";
 import Projects from "../Projects/Projects";
 import Profile from "../Profile/Profile";
-import Tech from "../Tech/Tech";
+// import Tech from "../Tech/Tech";
 
 function Main() {
   return (
     <div className="main">
       <Switch>
         {/* <Profile /> */}
-        <Route exact path="/" component={Profile} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/profile" component={Profile} />
+        {/* <Route exact path="/" component={Profile} /> */}
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/profile" component={Profile} />
       </Switch>
     </div>
   );
